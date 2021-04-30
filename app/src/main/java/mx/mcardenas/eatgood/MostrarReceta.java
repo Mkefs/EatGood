c3d8965246e07e5a159dcd0379198f3f9fa0ba56
Carl-Ang <cadaanca@gmail.com>
2021-04-30 13:12:55 -0500

    Solo añadi la clase Mostrar receta

---
+++ app/src/main/java/mx/mcardenas/eatgood/MostrarReceta.java
@@ -0,0 +1,14 @@
+package mx.mcardenas.eatgood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarReceta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_receta);
+    }
+}

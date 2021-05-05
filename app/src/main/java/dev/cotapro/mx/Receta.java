94c4af247edfcfad10160db0564405f5920ba30c
Carl-Ang <cadaanca@gmail.com>
2021-05-04 20:02:55 -0500

    Añadi el Activity Recetas

---
+++ app/src/main/java/dev/cotapro/mx/Receta.java
@@ -0,0 +1,14 @@
+package dev.cotapro.mx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Receta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receta);
+    }
+}

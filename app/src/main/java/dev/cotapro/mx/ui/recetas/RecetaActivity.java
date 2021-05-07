70fad6980eb0973a2fdfe31a9d65d83f2b4f4c23
AdairAAFS <afernandez0@ucol.mx>
2021-05-06 21:13:46 -0500

    Anadi boton para cambiar de fragment a activity

--- app/src/main/java/dev/cotapro/mx/ui/recetas/RecetaActivity.java
+++ app/src/main/java/dev/cotapro/mx/ui/recetas/RecetaActivity.java
@@ -2,19 +2,27 @@ package dev.cotapro.mx.ui.recetas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import dev.cotapro.mx.MainActivity;
 import dev.cotapro.mx.R;
+import dev.cotapro.mx.ui.home.HomeFragment;
 
 public class RecetaActivity extends AppCompatActivity {
 
     TextView textView, textView0, textView1;
     ImageView imageView;
+    private Object View;
+    Button btnSwitch2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +42,10 @@ public class RecetaActivity extends AppCompatActivity {
         textView0.setMovementMethod(new ScrollingMovementMethod());
         textView1.setMovementMethod(new ScrollingMovementMethod());
 
+    }
+    //Metodo para el boton anterior
+    public void Anterior(android.view.View view) {
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
     }
 }

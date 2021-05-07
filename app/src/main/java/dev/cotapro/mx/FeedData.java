757c955f79267c7d5b16f7d814fc0547a7fc052a
Mkefss <Mauricio.291202@gmail.com>
2021-05-07 16:56:59 -0500

    builder

--- app/src/main/java/dev/cotapro/mx/FeedData.java
+++ app/src/main/java/dev/cotapro/mx/FeedData.java
@@ -1,5 +1,18 @@
 package dev.cotapro.mx;
 
+import retrofit2.Retrofit;
+
 public class FeedData {
+	private static boolean started = false;
+	public static Retrofit kiwilimon;
+	public static Retrofit ingredientes;
+
+	public void init() {
+		if(started)
+			return;
+		kiwilimon = new Retrofit.Builder()
+			.baseUrl("")
+			.build();
+	}
 
 }

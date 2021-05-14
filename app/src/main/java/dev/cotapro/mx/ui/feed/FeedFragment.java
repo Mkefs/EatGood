2b5d1a0704da88e79b38d5802ed626665cf0f20a
Denzel <ddelarosa0@ucol.mx>
2021-05-14 12:50:36 -0500

    a

--- app/src/main/java/dev/cotapro/mx/ui/feed/FeedFragment.java
+++ app/src/main/java/dev/cotapro/mx/ui/feed/FeedFragment.java
@@ -11,7 +11,9 @@ import androidx.fragment.app.Fragment;
 
 import dev.cotapro.mx.R;
 
-public class FeedFragment extends Fragment {
+public class
+
+FeedFragment extends Fragment {
 	View vista;
 	@Nullable
 	@Override

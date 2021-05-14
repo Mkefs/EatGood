ce5e4bde0ecf0cfb24e8c6e4c7dc58d0487e6f22
Mkefss <Mauricio.291202@gmail.com>
2021-05-14 12:43:59 -0500

    Arreglado todo

--- app/src/main/java/dev/cotapro/mx/FeedData.java
+++ app/src/main/java/dev/cotapro/mx/FeedData.java
@@ -2,6 +2,8 @@ package dev.cotapro.mx;
 
 import android.content.Context;
 import androidx.room.Room;
+
+import dev.cotapro.mx.api_ingredientes.ApiManagement;
 import dev.cotapro.mx.recetas.GuardadosDB;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -10,8 +12,8 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public class FeedData {
 	private static boolean started = false;
 	public static GuardadosDB db;
-	public static Retrofit kiwilimon;
-	public static Retrofit ingredientes;
+	public static dev.cotapro.mx.api.ApiManagement.API_INTERACTION kiwilimon;
+	public static ApiManagement.API_INTERACION ingredientes;
 
 	public static void init(Context ctx) {
 		if(started)
@@ -19,14 +21,16 @@ public class FeedData {
 		kiwilimon = new Retrofit.Builder()
 				.addConverterFactory(ScalarsConverterFactory.create())
 				.addConverterFactory(GsonConverterFactory.create())
-				.baseUrl("https://gr.kiwilimon.com/v6")
-				.build();
+				.baseUrl("https://gr.kiwilimon.com/v6/")
+				.build()
+				.create(dev.cotapro.mx.api.ApiManagement.API_INTERACTION.class);
 		ingredientes = new Retrofit.Builder()
 				.addConverterFactory(ScalarsConverterFactory.create())
 				.addConverterFactory(GsonConverterFactory.create())
 				.baseUrl("https://ingredients-eatgood.000webhostapp.com/")
-				.build();
-    db = Room.databaseBuilder(ctx,
+				.build()
+				.create(ApiManagement.API_INTERACION.class);
+		db = Room.databaseBuilder(ctx,
 			GuardadosDB.class, "Recetas_Guardadas")
 			.build();
 		started = true;

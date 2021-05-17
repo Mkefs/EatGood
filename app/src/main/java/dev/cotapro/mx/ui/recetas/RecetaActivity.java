dc64d79215baa623d3de978f5723744745710419
Mkefss <Mauricio.291202@gmail.com>
2021-05-17 11:22:56 -0500

    Dev

--- app/src/main/java/dev/cotapro/mx/ui/recetas/RecetaActivity.java
+++ app/src/main/java/dev/cotapro/mx/ui/recetas/RecetaActivity.java
@@ -18,29 +18,10 @@ import dev.cotapro.mx.ui.home.HomeFragment;
 
 public class RecetaActivity extends AppCompatActivity {
 
-    TextView textView, textView0, textView1;
-    ImageView imageView;
-    private Object View;
-    Button btnSwitch2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.receta_vista);
-
-        imageView = findViewById(R.id.imgPlatilloPrueba);
-
-        String url="https://media-adsa.camilyo.software/media-adsa/static/3228/270.png";
-        Glide.with(this)
-                .load(url).placeholder(R.drawable.ic_launcher_background).error(R.drawable.ic_launcher_background).into(imageView);
-
-        textView = findViewById(R.id.txtIngredientes);
-        textView0 = findViewById(R.id.txtPreparacion);
-        textView1 = findViewById(R.id.txtDescripcion);
-
-        textView.setMovementMethod(new ScrollingMovementMethod());
-        textView0.setMovementMethod(new ScrollingMovementMethod());
-        textView1.setMovementMethod(new ScrollingMovementMethod());
 
     }
     //Metodo para el boton anterior

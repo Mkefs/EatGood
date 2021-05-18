395b4c34f4c754618b020d74f54a4dcb4747b15a
moonreal <pjaime@gmail.com>
2021-05-18 12:26:15 -0500

    hola

--- app/src/main/java/dev/cotapro/mx/ui/search/SearchFragment.java
+++ app/src/main/java/dev/cotapro/mx/ui/search/SearchFragment.java
@@ -31,6 +31,7 @@ public class SearchFragment extends Fragment {
 			@Override
 			public void run() {
 				String[][] ingredientes = Datos.getnombres(FeedData.ingredientes);
+				//Provecho guapo 
 			}
 		});
 

ad08c58da47e525b3d6aaa76a56531ecd669740f
Mkefss <Mauricio.291202@gmail.com>
2021-05-21 23:29:11 -0500

    Yea

--- app/src/main/java/dev/cotapro/mx/ui/feed/ListAdapter.java
+++ app/src/main/java/dev/cotapro/mx/ui/feed/ListAdapter.java
@@ -1,9 +1,11 @@
 package dev.cotapro.mx.ui.feed;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,20 +16,20 @@ import java.util.ArrayList;
 
 import dev.cotapro.mx.R;
 import dev.cotapro.mx.api.Descripcion;
+import dev.cotapro.mx.ui.recetas.RecetaActivity;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
     private final ArrayList<Descripcion> mData;
 
     public ListAdapter(Descripcion[] itemList) {
         this.mData = new ArrayList<>();
-        for(int i = 0; i < itemList.length; i++) {
-            if(!itemList[i].key.isEmpty()) {
-                mData.add(itemList[i]);
+        for (Descripcion descripcion : itemList) {
+            if (!descripcion.key.isEmpty()) {
+                mData.add(descripcion);
             }
         }
     }
 
-    @NonNull
     @Override
     public int getItemCount(){
         return mData.size();
@@ -62,13 +64,21 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
             String image_url = "https://cdn.kiwilimon.com/recetaimagen/%s/%s";
             image_url = String.format(image_url, item.key, item.image);
 
+            platillo.setText(item.name);
+            autor.setText(item.chef);
+            stars.setText(item.rating);
             Glide.with(itemView)
                 .load(image_url)
                 .placeholder(R.drawable.ic_lunch_dining_black_24dp)
                 .into(iconImage);
-            platillo.setText(item.name);
-            autor.setText(item.chef);
-            stars.setText(item.rating);
+
+            LinearLayout container = itemView.findViewById(R.id.feedContainer);
+            container.setOnClickListener(v -> {
+                Bundle extras = new Bundle();
+                extras.putInt("id", Integer.parseInt(item.key));
+                extras.putBoolean("internet", false);
+                RecetaActivity.open_act(itemView.getContext(), extras);
+            });
         }
 
     }
--- app/src/main/java/dev/cotapro/mx/ui/recetas/RecetaActivity.java
+++ app/src/main/java/dev/cotapro/mx/ui/recetas/RecetaActivity.java
@@ -1,7 +1,6 @@
 package dev.cotapro.mx.ui.recetas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,6 +8,8 @@ import android.os.Looper;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
@@ -19,7 +20,7 @@ import java.util.concurrent.Executors;
 import dev.cotapro.mx.FeedData;
 import dev.cotapro.mx.MainActivity;
 import dev.cotapro.mx.R;
-import dev.cotapro.mx.api.Receta;
+import dev.cotapro.mx.recetas.Receta;
 
 public class RecetaActivity extends AppCompatActivity {
     String json;
@@ -27,7 +28,7 @@ public class RecetaActivity extends AppCompatActivity {
             favoritosReceta, ratingReceta, dateReceta, ingredientesReceta, pasosReceta, nombreChef;
     ImageView imageView;
     int id = 0;
-    boolean saved = true;
+    boolean internet = true;
 
 
 
@@ -36,87 +37,88 @@ public class RecetaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.receta_vista);
         Bundle b = getIntent().getExtras();
-        tituloReceta=findViewById(R.id.txtTitulo);
-        caloriasReceta=findViewById(R.id.txtCalorias);
-        preparacionReceta=findViewById(R.id.txtTPreparacion);
-        tcoccionReceta=findViewById(R.id.txtTCoccion);
-        descripcionReceta=findViewById(R.id.txtDescripcion);
-        favoritosReceta=findViewById(R.id.txtFavoritos);
-        ratingReceta=findViewById(R.id.txtRating);
-        dateReceta=findViewById(R.id.txtDate);
-        ingredientesReceta=findViewById(R.id.txtIngredientes);
-        pasosReceta=findViewById(R.id.txtPasos);
-        nombreChef=findViewById(R.id.txtNombre);
+        tituloReceta = findViewById(R.id.txtTitulo);
+        caloriasReceta = findViewById(R.id.txtCalorias);
+        preparacionReceta = findViewById(R.id.txtTPreparacion);
+        tcoccionReceta = findViewById(R.id.txtTCoccion);
+        descripcionReceta = findViewById(R.id.txtDescripcion);
+        favoritosReceta = findViewById(R.id.txtFavoritos);
+        ratingReceta = findViewById(R.id.txtRating);
+        dateReceta = findViewById(R.id.txtDate);
+        ingredientesReceta = findViewById(R.id.txtIngredientes);
+        pasosReceta = findViewById(R.id.txtPasos);
+        nombreChef = findViewById(R.id.txtNombre);
+
         if(b != null) {
             id = b.getInt("id");
-            saved = b.getBoolean("saved");
+            internet = b.getBoolean("internet");
 
             Executor executor = Executors.newSingleThreadExecutor();
             Handler handler = new Handler(Looper.getMainLooper());
 
             executor.execute(() -> {
-                if(saved)
-                    json = FeedData.db.recetas().getReceta(id);
-                else
-                    json = FeedData.get_recipe(id);
-
-                // Lo conviertes a una variable tipo receta
                 Gson gson = new Gson();
-                Receta receta = gson.fromJson(json, Receta.class);
+
+                if(internet) {
+                    Receta receta_guardada = FeedData.db.recetas().get_receta(id);
+                    json = receta_guardada.json;
+                } else {
+                    json = FeedData.get_recipe(id);
+                }
+                dev.cotapro.mx.api.Receta recipe =
+                    gson.fromJson(json, dev.cotapro.mx.api.Receta.class);
 
                 //Convertimos los int a string necesarios
-                String caloriasRecetaString = String.valueOf(receta.calories);
-                String tcoccionRecetaString = String.valueOf(receta.cooktime);
-                String favoritosRecetaString = String.valueOf(receta.favorites);
-                String ratingreceta = String.valueOf(receta.raiting);
+                String caloriasRecetaString = String.valueOf(recipe.calories);
+                String tcoccionRecetaString = String.valueOf(recipe.cooktime);
+                String favoritosRecetaString = String.valueOf(recipe.favorites);
+                String ratingreceta = String.valueOf(recipe.raiting);
 
-
-                //Aqui se hace lo de la vista
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        //Aqui se hace lo de los TextView
-                        tituloReceta.setText(receta.titleh1);
-                        caloriasReceta.setText(caloriasRecetaString);
-                        if(receta.cooked == 1) {
-                            preparacionReceta.setText("Si");
-                        } else {
-                            preparacionReceta.setText("No");
-                        }
-                        tcoccionReceta.setText(tcoccionRecetaString);
-                        descripcionReceta.setText(receta.description+"\n");
-                        favoritosReceta.setText(favoritosRecetaString);
-                        ratingReceta.setText(ratingreceta);
-                        dateReceta.setText(receta.published);
-                        for(int i = 0; i < receta.ingredients.length; i++) {
-                            String texto = (String) ingredientesReceta.getText();
-                            ingredientesReceta.setText(texto + " - " +receta.ingredients[i].text+"\n");
-                        }
-                        for(int i = 0; i < receta.steps.length; i++) {
-                            String texto = (String) pasosReceta.getText();
-                            pasosReceta.setText(texto + " - " +receta.steps[i].text+"\n");
-                        }
-
-                        nombreChef.setText(receta.images[0].clientdata.firstname+" "+receta.images[0].clientdata.lastname);
-                        //En las siguientes 5 lineas, se hace lo de la imagen desde internet (cabe resaltar que en si, son 3 lineas pero por estetica le di saltos de linea)
-                        imageView = findViewById(R.id.imgReceta);
-                        String context= "https://cdn.kiwilimon.com/recetaimagen/"+receta.key+"/"+receta.image;
-                        Glide.with(imageView)
-                                .load(context).placeholder(R.drawable.ic_launcher_background)
-                                .error(R.drawable.ic_launcher_background).into(imageView);
-
-                        /*descripcionReceta.setMovementMethod(new ScrollingMovementMethod());
-                        ingredientesReceta.setMovementMethod(new ScrollingMovementMethod());
-                        pasosReceta.setMovementMethod(new ScrollingMovementMethod());*/
-
+                // Aqui se hace lo de la vista
+                handler.post(() -> {
+                    //Aqui se hace lo de los TextView
+                    tituloReceta.setText(recipe.titleh1);
+                    caloriasReceta.setText(caloriasRecetaString);
+                    if(recipe.cooked == 1) {
+                        preparacionReceta.setText("Si");
+                    } else {
+                        preparacionReceta.setText("No");
                     }
+                    tcoccionReceta.setText(tcoccionRecetaString);
+                    descripcionReceta.setText(recipe.description+"\n");
+                    favoritosReceta.setText(favoritosRecetaString);
+                    ratingReceta.setText(ratingreceta);
+                    dateReceta.setText(recipe.published);
+                    for(int i = 0; i < recipe.ingredients.length; i++) {
+                        String texto = (String) ingredientesReceta.getText();
+                        ingredientesReceta.setText(texto + " - " +recipe.ingredients[i].text+"\n");
+                    }
+                    for(int i = 0; i < recipe.steps.length; i++) {
+                        String texto = (String) pasosReceta.getText();
+                        pasosReceta.setText(texto + " - " +recipe.steps[i].text+"\n");
+                    }
+
+                    nombreChef.setText(recipe.images[0].clientdata.firstname
+                        +" "+recipe.images[0].clientdata.lastname);
+                    //En las siguientes 5 lineas, se hace lo de la imagen desde internet
+                    // (cabe resaltar que en si, son 3 lineas pero por estetica le di saltos de linea)
+                    imageView = findViewById(R.id.imgReceta);
+                    String context= "https://cdn.kiwilimon.com/recetaimagen/"+recipe.key+"/"+recipe.image;
+                    Glide.with(imageView)
+                            .load(context).placeholder(R.drawable.ic_launcher_background)
+                            .error(R.drawable.ic_launcher_background).into(imageView);
                 });
             });
         } else {
             Toast.makeText(RecetaActivity.this, "Error al conseguir los datos", Toast.LENGTH_LONG).show();
-            // Error al conseguir datos
         }
+    }
 
+    // Metodo para acceder a esta vista
+    public static void open_act(Context context, Bundle extras) {
+        Intent siguiente = new Intent(context, RecetaActivity.class);
+        siguiente.putExtras(extras);
+        context.startActivity(siguiente);
     }
 
     //Metodo para el boton anterior
